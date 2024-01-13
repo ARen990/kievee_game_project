ef4649df67b9e2891bf6b60beecb3ec8aa84b213
Krittimon Promsanar <krittimonp28@gmail.com>
2024-01-14 02:46:54 +0700

    change size font and button

--- why_R_U_runnig/home.py
+++ why_R_U_runnig/home.py
@@ -11,21 +11,21 @@ class StartScreen(Screen):
         super(StartScreen, self).__init__(**kwargs)
 
         # add image
-        middle_image = Image(source='image/stickman/stickrun2.png', size_hint=(None, None), size=(250, 250),pos=(Window.width / 2 - 140, Window.height / 2 - 90))
+        middle_image = Image(source='image/stickman/stickrun2.png', size_hint=(None, None), size=(300, 300),pos=(Window.width / 2 - 140, Window.height / 2 ))
         self.add_widget(middle_image)
 
         # add text name game
         about_text = Label(
-            text="[color=FF9BD2]Why Are U Running[/color]",font_size=50, size_hint=(None, None),pos=(Window.width / 2 - 50, 200),markup=True)
+            text="[color=FF9BD2]Why Are U Running[/color]",font_size=100, size_hint=(None, None),pos=(Window.width / 2 - 50, 300),markup=True)
         self.add_widget(about_text)
 
         # add start button
-        start_button = Button(text="Start", size_hint=(None, None), size=(100, 50), pos=(Window.width / 2 - 50, 85))
+        start_button = Button(text="Start",font_size=50, size_hint=(None, None), size=(150, 100), pos=(Window.width / 2 - 50, 150))
         start_button.bind(on_press=self.start_game)
         self.add_widget(start_button)
 
         # add about button
-        about_button = Button(text="About", size_hint=(None, None), size=(100, 50), pos=(Window.width / 2 - 50, 30))
+        about_button = Button(text="About", font_size=50,  size_hint=(None, None), size=(150, 100), pos=(Window.width / 2 - 50, 30))
         about_button.bind(on_press=self.switch_to_about)
         self.add_widget(about_button)
 
@@ -42,32 +42,32 @@ class About(Screen):
         super(About, self).__init__(**kwargs)
 
         # add back button
-        back_button = Button(text="<<Back", size_hint=(None, None), size=(100, 50), pos=(100, 400))
+        back_button = Button(text="<<Back",  font_size=40, size_hint=(None, None), size=(150, 100), pos=(Window.width - 1000, 650))
         back_button.bind(on_press=self.go_back)
         self.add_widget(back_button)
 
         # add text
         about_text = Label(
-            text="[color=FF9BD2]Contact channels[/color]",font_size=50,pos=(0, 100),markup=True)
+            text="[color=FF9BD2]Contact channels[/color]",font_size=100,pos=(0, 200),markup=True)
         self.add_widget(about_text)
 
         # add twitter button
-        twitter_button = Button(text="Twitter", size_hint=(None, None), size=(100, 50), pos=(Window.width / 2 - 50, 200))
+        twitter_button = Button(text="Twitter", font_size=50, size_hint=(None, None), size=(300, 100), pos=(Window.width / 2 - 50, 350))
         twitter_button.bind(on_press=lambda x: self.open_link("https://x.com/Aenijin?s=20"))
         self.add_widget(twitter_button)
 
         # add Instagram button
-        instagram_button = Button(text="Instagram", size_hint=(None, None), size=(100, 50), pos=(Window.width / 2 - 50, 150))
+        instagram_button = Button(text="Instagram", font_size=50, size_hint=(None, None), size=(300, 100), pos=(Window.width / 2 - 50, 250))
         instagram_button.bind(on_press=lambda x: self.open_link("https://www.instagram.com/dragon_sutdy?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="))
         self.add_widget(instagram_button)
 
         # add GitHub button
-        github_button = Button(text="GitHub", size_hint=(None, None), size=(100, 50), pos=(Window.width / 2 - 50, 100))
+        github_button = Button(text="GitHub", font_size=50, size_hint=(None, None), size=(300, 100), pos=(Window.width / 2 - 50, 150))
         github_button.bind(on_press=lambda x: self.open_link("https://github.com/ARen990"))
         self.add_widget(github_button)
 
         # add youtube button
-        youtube_button = Button(text="YouTube", size_hint=(None, None), size=(100, 50), pos=(Window.width / 2 - 50, 50))
+        youtube_button = Button(text="YouTube", font_size=50, size_hint=(None, None), size=(300, 100), pos=(Window.width / 2 - 50, 50))
         youtube_button.bind(on_press=lambda x: self.open_link("https://www.youtube.com/channel/UCHbOa_h7HDl3jTByZGTwKVw"))
         self.add_widget(youtube_button)
 
